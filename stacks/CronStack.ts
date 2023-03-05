581c3777e26a15c5e321b025bfd5b2e16c3d098c
deputyuniverse <gideonbrooke@tutanota.com>
2023-03-05 19:22:25 +0000

    Tidy up

--- stacks/CronStack.ts
+++ stacks/CronStack.ts
@@ -1,6 +1,9 @@
 import { Cron, StackContext, use } from "@serverless-stack/resources";
+import { LayerVersion } from "aws-cdk-lib/aws-lambda";
 
 export function CronStack({ stack }: StackContext) {
+
+  stack.addDefaultFunctionLayers([LayerVersion.fromLayerVersionArn(stack, "ChromeLayer", "arn:aws:lambda:us-east-1:764866452798:layer:chrome-aws-lambda:22")])
 
   const cron = new Cron(stack, "Cron", {
     schedule: "rate(1 minute)",
--- stacks/index.ts
+++ stacks/index.ts
@@ -1,7 +1,6 @@
 import { CronStack } from "./CronStack";
 import { DatabaseStack } from "./DatabaseStack";
 import { App } from "@serverless-stack/resources";
-import { LayerVersion } from "aws-cdk-lib/aws-lambda";
 
 export default function (app: App) {
   app.setDefaultFunctionProps({
@@ -11,9 +10,5 @@ export default function (app: App) {
       format: "esm",
     },
   });
-  app.addDefaultFunctionLayers([LayerVersion.fromLayerVersionArn(app, "ChromeLayer", "arn:aws:lambda:us-east-1:764866452798:layer:chrome-aws-lambda:22")])
-  if (app.stage === "dev") {
-    app.setDefaultRemovalPolicy("destroy");
-  }
   app.stack(DatabaseStack).stack(CronStack);
 }
